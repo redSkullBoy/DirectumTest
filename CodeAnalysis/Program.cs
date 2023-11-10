5546054102ed02fd6f03b389c75a970578afcfbd
dantonov <ideana100@yandex.ru>
2023-11-10 20:30:10 +0300

    Добавьте файлы проекта.

---
+++ CodeAnalysis/Program.cs
@@ -0,0 +1,89 @@
+﻿using System.Globalization;
+
+namespace CodeAnalysis;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Сортировка, сортировка!");
+
+        var keyValuePair = new KeyValuePair<int, string>[]
+        {
+            new KeyValuePair<int, string>(3,"Tom3"),
+            new KeyValuePair<int, string>(4,"Tom4"),
+            new KeyValuePair<int, string>(1,"Tom1"),
+            new KeyValuePair<int, string>(2,"Tom2"),
+        };
+
+        //Func1(ref keyValuePair, 0, "Tom0"); //добавляет новый елемент и сортирует по ключу
+        //FuncMy(ref keyValuePair, 0, "Tom0");
+        Quicksort(ref keyValuePair, 0, "Tom0");
+
+        foreach (KeyValuePair<int, string> innerpair in keyValuePair)
+        {
+            Console.WriteLine(innerpair.Key + " " + innerpair.Value);
+        }
+    }
+
+    /// <summary>
+    /// Обратный метод пузырька
+    /// </summary>
+    /// <param name="a"></param>
+    /// <param name="key"></param>
+    /// <param name="value"></param>
+    static void Func1(ref KeyValuePair<int, string>[] a, int key, string value)
+    {
+        Array.Resize(ref a, a.Length + 1);
+
+        var keyValuePair = new KeyValuePair<int, string>(key, value);
+        a[a.Length - 1] = keyValuePair;
+
+        for (int i = 0; i < a.Length; i++)
+        {
+            for (int j = a.Length - 1; j > 0; j--)
+            {
+                if (a[j - 1].Key > a[j].Key)
+                {
+                    KeyValuePair<int, string> x;
+                    x = a[j - 1];
+                    a[j - 1] = a[j];
+                    a[j] = x;
+                }
+            }
+        }
+    }
+
+    /// <summary>
+    /// Если не хочеться париться)
+    /// </summary>
+    /// <param name="pairArray"></param>
+    /// <param name="key"></param>
+    /// <param name="value"></param>
+    static void FuncMy(ref KeyValuePair<int, string>[] pairArray, int key, string value)
+    {
+        Array.Resize(ref pairArray, pairArray.Length + 1);
+
+        var keyValuePair = new KeyValuePair<int, string>(key, value);
+        pairArray[pairArray.Length - 1] = keyValuePair;
+
+        Array.Sort(pairArray, (a, b) => a.Key.CompareTo(b.Key));
+    }
+
+    /// <summary>
+    /// Быстрая сортировка
+    /// </summary>
+    /// <param name="pairArray"></param>
+    /// <param name="key"></param>
+    /// <param name="value"></param>
+    public static void Quicksort(ref KeyValuePair<int, string>[] pairArray, int key, string value)
+    {
+        Array.Resize(ref pairArray, pairArray.Length + 1);
+
+        var keyValuePair = new KeyValuePair<int, string>(key, value);
+        pairArray[pairArray.Length - 1] = keyValuePair;
+
+        var quick = new QuickSort();
+        quick.Start(ref pairArray);
+    }
+}
---
+++ CodeAnalysis/QuickSort.cs
@@ -0,0 +1,51 @@
+﻿namespace CodeAnalysis;
+
+public class QuickSort
+{
+    //быстрая сортировка
+    public void Start(ref KeyValuePair<int, string>[] pairArray, int minIndex, int maxIndex)
+    {
+        if (minIndex >= maxIndex)
+        {
+            return;
+        }
+
+        var pivotIndex = Partition(ref pairArray, minIndex, maxIndex);
+        Start(ref pairArray, minIndex, pivotIndex - 1);
+        Start(ref pairArray, pivotIndex + 1, maxIndex);
+
+        return;
+    }
+
+    public void Start(ref KeyValuePair<int, string>[] pairArray)
+    {
+        Start(ref pairArray, 0, pairArray.Length - 1);
+        return;
+    }
+
+    //метод для обмена элементов массива
+    void Swap(ref KeyValuePair<int, string> x, ref KeyValuePair<int, string> y)
+    {
+        var t = x;
+        x = y;
+        y = t;
+    }
+
+    //метод возвращающий индекс опорного элемента
+    int Partition(ref KeyValuePair<int, string>[] pairArray, int minIndex, int maxIndex)
+    {
+        var pivot = minIndex - 1;
+        for (var i = minIndex; i < maxIndex; i++)
+        {
+            if (pairArray[i].Key < pairArray[maxIndex].Key)
+            {
+                pivot++;
+                Swap(ref pairArray[pivot], ref pairArray[i]);
+            }
+        }
+
+        pivot++;
+        Swap(ref pairArray[pivot], ref pairArray[maxIndex]);
+        return pivot;
+    }
+}
